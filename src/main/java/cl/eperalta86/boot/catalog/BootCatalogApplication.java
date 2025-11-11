ed51cbb6b15f6421580e94fb4716cb4c1a0ba83e
Esteban Peralta <esteban.peralta.galarce@gmail.com>
2025-11-11 12:30:30 -0300

    boot initial config

---
+++ src/main/java/cl/eperalta86/boot/catalog/BootCatalogApplication.java
@@ -0,0 +1,13 @@
+package cl.eperalta86.boot.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/eperalta86/boot/catalog/BootCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.eperalta86.boot.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
