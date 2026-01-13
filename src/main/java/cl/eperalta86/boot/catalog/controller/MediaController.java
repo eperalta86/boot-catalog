eccff5dc17b84c8343b31f43ef163985e56f3809
Esteban Peralta <esteban.peralta.galarce@gmail.com>
2026-01-13 13:15:58 -0300

    media test

---
+++ src/main/java/cl/eperalta86/boot/catalog/controller/MediaController.java
@@ -0,0 +1,33 @@
+package cl.eperalta86.boot.catalog.controller;
+
+import cl.eperalta86.boot.catalog.domain.MediaItem;
+import cl.eperalta86.boot.catalog.domain.MediaItem.MediaType; 
+import cl.eperalta86.boot.catalog.domain.MediaItem.MediaStatus;
+import cl.eperalta86.boot.catalog.service.MediaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/media")
+//Para angular.
+@CrossOrigin(origins = "http://localhost:4200") 
+public class MediaController {
+
+    private final MediaService service;
+
+    public MediaController(MediaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<MediaItem> getAll() {
+        return service.findAll();
+    }
+    public record CreateMediaRequest(String title, MediaType type, MediaStatus status) {}
+
+    @PostMapping
+    public MediaItem create(@RequestBody CreateMediaRequest request) {
+        return service.create(request.title(), request.type(), request.status());
+    }
+}
---
+++ src/main/java/cl/eperalta86/boot/catalog/service/MediaService.java
@@ -0,0 +1,33 @@
+package cl.eperalta86.boot.catalog.service;
+
+import cl.eperalta86.boot.catalog.domain.MediaItem;
+import cl.eperalta86.boot.catalog.domain.MediaItem.MediaType;
+import cl.eperalta86.boot.catalog.repository.MediaItemRepository;
+import cl.eperalta86.boot.catalog.domain.MediaItem.MediaStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MediaService {
+
+    private final MediaItemRepository repository;
+
+    public MediaService(MediaItemRepository repository) {
+        this.repository = repository;
+    }
+
+    // Read-only optimiza el rendimiento en algunas BD
+    @Transactional(readOnly = true) 
+    public List<MediaItem> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public MediaItem create(String title, MediaType type, MediaStatus status) {
+
+        MediaItem newItem = new MediaItem(title, type, status);
+        return repository.save(newItem);
+    }
+}
